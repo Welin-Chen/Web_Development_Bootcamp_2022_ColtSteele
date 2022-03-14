891223d86ad1409b8cb52ced96a1262bb7472632
welin <a0929015591@gmail.com>
2022-03-14 16:02:47 +0800

    Adding Basic Styles

--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const cities = require("./cities");
 const { places, descriptors } = require("./seedHelpers");
 const Campground = require("../models/campground");
+const axios = require("axios");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp");
 
@@ -13,13 +14,35 @@ db.once("open", () => {
 
 const sample = (array) => array[Math.floor(Math.random() * array.length)];
 
+// call unsplash and return small image
+async function seedImg() {
+  try {
+    const api = "https://api.unsplash.com/";
+    const res = await axios.get(api + "photos/random", {
+      params: {
+        client_id: "a2dTIMkrrP21IMz7KT19hoWXUl-25g0jcpwdc9TJLrM",
+        collections: 1114848,
+      },
+    });
+    return res.data.urls.small;
+  } catch (e) {
+    console.log(e);
+  }
+}
+
 const seedDB = async () => {
   await Campground.deleteMany({});
   for (let i = 0; i < 50; i++) {
     const rand1000 = Math.floor(Math.random() * 1000);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251",
+      // image: await seedImg(),
+      description:
+        "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Iste nisi suscipit eaque pariatur magnam qui laboriosam, id maiores recusandae unde quod saepe velit ipsa cum, voluptatem sed maxime deserunt! Doloremque!",
+      price,
     });
     await camp.save();
   }

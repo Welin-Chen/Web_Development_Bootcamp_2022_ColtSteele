fd903aa65c265c91216d3ab9e93cc59db436f24c
welin <a0929015591@gmail.com>
2022-03-21 03:04:16 +0800

    Mongo Relationships with Express

---
+++ Ch.45_Mongo_Relationships_with_Express/DemoApp/index.js
@@ -0,0 +1,131 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+const mongoose = require("mongoose");
+const methodOverride = require("method-override");
+
+const Product = require("./models/product");
+const Farm = require("./models/farm");
+const categories = ["fruit", "vegetable", "dairy"];
+
+mongoose
+  .connect("mongodb://localhost:27017/farmStandTake2")
+  .then(() => {
+    console.log("Mongo database connection open!");
+  })
+  .catch((err) => {
+    console.log("Mong database connection error!");
+  });
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+
+// Farm Routes
+app.get("/farms", async (req, res) => {
+  const farms = await Farm.find({});
+  res.render("farms/index", { farms });
+});
+
+app.delete("/farms/:id", async (req, res) => {
+  console.log("Deleting");
+  const farm = await Farm.findByIdAndDelete(req.params.id);
+  res.redirect("/farms");
+});
+
+app.get("/farms/new", (req, res) => {
+  res.render("farms/new");
+});
+
+app.get("/farms/:id", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id).populate("products");
+  res.render("farms/show", { farm });
+});
+
+app.post("/farms", async (req, res) => {
+  const farm = new Farm(req.body);
+  await farm.save();
+  res.redirect("farms");
+});
+
+app.get("/farms/:id/products/new", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  res.render("products/new", { categories, farm });
+});
+
+app.post("/farms/:id/products", async (req, res) => {
+  const { id } = req.params;
+  const farm = await Farm.findById(id);
+  const { name, price, category } = req.body;
+  const product = new Product({ name, price, category });
+  farm.products.push(product);
+  product.farm = farm;
+  await farm.save();
+  await product.save();
+  res.redirect(`/farms/${id}`);
+});
+// Product Routes
+
+// Read(1)
+app.get("/products", async (req, res) => {
+  const { category } = req.query;
+  if (category) {
+    const products = await Product.find({ category });
+    res.render("products/index", { products, category });
+  } else {
+    const products = await Product.find({});
+    res.render("products/index", { products, category: "All" });
+  }
+});
+
+// Create get form(1)
+app.get("/products/new", (req, res) => {
+  res.render("products/new", { categories });
+});
+
+// Create post form(2)
+app.post("/products", async (req, res) => {
+  const newProduct = new Product(req.body);
+  await newProduct.save();
+  res.redirect(`/products/${newProduct._id}`);
+});
+
+// Read(2)
+app.get("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id).populate("farm", "name");
+  res.render("products/show", { product });
+});
+
+// Update get form(1)
+app.get("/products/:id/edit", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("products/edit", { product, categories });
+});
+
+// Upadte put form(2)
+app.put("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findByIdAndUpdate(id, req.body, {
+    runValiators: true,
+    new: true,
+  });
+  product.save();
+  res.redirect(`/products/${product._id}`);
+});
+
+// Delete
+app.delete("/products/:id", async (req, res) => {
+  const { id } = req.params;
+  const deleteProduct = await Product.findByIdAndDelete(id);
+  res.redirect("/products");
+});
+
+app.listen(3000, () => {
+  console.log("App is listening on port 3000");
+});
---
+++ Ch.45_Mongo_Relationships_with_Express/DemoApp/seeds.js
@@ -0,0 +1,62 @@
+const mongoose = require("mongoose");
+
+const Product = require("./models/product");
+
+mongoose
+  .connect("mongodb://localhost:27017/farmStandTake2")
+  .then(() => {
+    console.log("Mongo database connection open!");
+  })
+  .catch((err) => {
+    console.log("Mong database connection error!");
+  });
+
+// const p = new Product({
+//   name: "Grapefruit",
+//   price: 1.99,
+//   category: "fruit",
+// });
+
+// p.save()
+//   .then((p) => {
+//     console.log(p);
+//   })
+//   .catch((e) => {
+//     console.log(e);
+//   });
+
+const seedProducts = [
+  {
+    name: "Eggplant",
+    price: 1.0,
+    category: "vegetable",
+  },
+  {
+    name: "Melon",
+    price: 4.99,
+    category: "fruit",
+  },
+  {
+    name: "Watermelon",
+    price: 3.99,
+    category: "fruit",
+  },
+  {
+    name: "Celery",
+    price: 1.52,
+    category: "vegetable",
+  },
+  {
+    name: "Milk",
+    price: 2.68,
+    category: "dairy",
+  },
+];
+
+Product.insertMany(seedProducts)
+  .then((res) => {
+    console.log(res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });

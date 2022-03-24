3ea4e5d4e32ac79f8e8d42285a613ac5295c352c
welin <a0929015591@gmail.com>
2022-03-24 21:28:03 +0800

    YelpCamp: Restructuring & Flash

---
+++ YelpCamp/rotues/campgrounds.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const router = express.Router();
+const catchAsync = require("../utils/catchAsync");
+const { campgroundSchema } = require("../schemas");
+const ExpressError = require("../utils/ExpressError");
+const Campground = require("../models/campground");
+
+const validateCampground = (req, res, next) => {
+  const { error } = campgroundSchema.validate(req.body);
+  if (error) {
+    const msg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(msg, 400);
+  } else {
+    next();
+  }
+};
+
+// Read all
+router.get(
+  "/",
+  catchAsync(async (req, res) => {
+    const campgrounds = await Campground.find({});
+    res.render("campgrounds/index", { campgrounds });
+  })
+);
+
+// Create form
+router.get("/new", (req, res) => {
+  res.render("campgrounds/new");
+});
+
+// Create form post
+router.post(
+  "/",
+  validateCampground,
+  catchAsync(async (req, res, next) => {
+    // if (!req.body.campground)
+    // throw new ExpressError("Invalid Campground Data", 400);
+
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    req.flash("success", "Successfully made a new campground!");
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+// Update form
+router.get(
+  "/:id/edit",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    if (!campground) {
+      req.flash("error", "Cannot find the campground and update!");
+      return res.redirect("/campgrounds");
+    }
+    res.render("campgrounds/edit", { campground });
+  })
+);
+
+// Update form post
+router.put(
+  "/:id",
+  validateCampground,
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, {
+      ...req.body.campground,
+    });
+    await campground.save();
+    req.flash("success", "Succesfully updated campground!");
+    res.redirect(`/campgrounds/${campground._id}`);
+  })
+);
+
+// Read detail
+router.get(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const campground = await Campground.findById(req.params.id).populate(
+      "reviews"
+    );
+    if (!campground) {
+      req.flash("error", "Cannot find the campground!");
+      return res.redirect("/campgrounds");
+    }
+    res.render("campgrounds/show", { campground });
+  })
+);
+
+// Delete
+router.delete(
+  "/:id",
+  catchAsync(async (req, res) => {
+    const { id } = req.params;
+    await Campground.findByIdAndDelete(id);
+    req.flash("success", "Successfully deleted campground!");
+    res.redirect("/campgrounds");
+  })
+);
+
+module.exports = router;

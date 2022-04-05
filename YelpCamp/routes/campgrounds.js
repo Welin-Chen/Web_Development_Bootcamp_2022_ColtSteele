3912ef7a171994b5b51822ea30813173b10ace7e
welin <a0929015591@gmail.com>
2022-04-06 03:45:05 +0800

    YelpCamp: Basic Authorization

--- YelpCamp/routes/campgrounds.js
+++ YelpCamp/routes/campgrounds.js
@@ -1,21 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const catchAsync = require("../utils/catchAsync");
-const { campgroundSchema } = require("../schemas");
-const { isLoggedIn } = require("../middleware");
-
-const ExpressError = require("../utils/ExpressError");
+const { isLoggedIn, isAuthor, validateCampground } = require("../middleware");
 const Campground = require("../models/campground");
-
-const validateCampground = (req, res, next) => {
-  const { error } = campgroundSchema.validate(req.body);
-  if (error) {
-    const msg = error.details.map((el) => el.message).join(",");
-    throw new ExpressError(msg, 400);
-  } else {
-    next();
-  }
-};
 
 // Read all
 router.get(
@@ -26,33 +13,33 @@ router.get(
   })
 );
 
-// Create form
+// Create camrpground
 router.get("/new", isLoggedIn, (req, res) => {
   res.render("campgrounds/new");
 });
 
-// Create form post
+// Create camrpground post
 router.post(
   "/",
   isLoggedIn,
   validateCampground,
   catchAsync(async (req, res, next) => {
-    // if (!req.body.campground)
-    // throw new ExpressError("Invalid Campground Data", 400);
-
     const campground = new Campground(req.body.campground);
+    campground.author = req.user._id;
     await campground.save();
     req.flash("success", "Successfully made a new campground!");
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
-// Update form
+// Update camrpground
 router.get(
   "/:id/edit",
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id);
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
     if (!campground) {
       req.flash("error", "Cannot find the campground and update!");
       return res.redirect("/campgrounds");
@@ -61,29 +48,35 @@ router.get(
   })
 );
 
-// Update form post
+// Update camrpground post
 router.put(
   "/:id",
   isLoggedIn,
+  isAuthor,
   validateCampground,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findByIdAndUpdate(id, {
       ...req.body.campground,
     });
-    await campground.save();
     req.flash("success", "Succesfully updated campground!");
     res.redirect(`/campgrounds/${campground._id}`);
   })
 );
 
-// Read detail
+// Read camrpground detail
 router.get(
   "/:id",
   catchAsync(async (req, res) => {
-    const campground = await Campground.findById(req.params.id).populate(
-      "reviews"
-    );
+    const campground = await Campground.findById(req.params.id)
+      // .populate("reviews")
+      .populate({
+        path: "reviews",
+        populate: {
+          path: "author",
+        },
+      })
+      .populate("author");
     if (!campground) {
       req.flash("error", "Cannot find the campground!");
       return res.redirect("/campgrounds");
@@ -92,10 +85,11 @@ router.get(
   })
 );
 
-// Delete
+// Delete camrpground
 router.delete(
   "/:id",
   isLoggedIn,
+  isAuthor,
   catchAsync(async (req, res) => {
     const { id } = req.params;
     await Campground.findByIdAndDelete(id);
--- YelpCamp/seeds/index.js
+++ YelpCamp/seeds/index.js
@@ -36,6 +36,7 @@ const seedDB = async () => {
     const rand1000 = Math.floor(Math.random() * 1000);
     const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
+      author: "624b2f54035a01795e4a0854",
       location: `${cities[rand1000].city}, ${cities[rand1000].state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
       image: "https://source.unsplash.com/collection/483251",
